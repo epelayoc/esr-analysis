380a3bd3d9220fada00022a8ea7b1e80727dac3d
epelayoc <97033678+epelayoc@users.noreply.github.com>
2026-08-14 14:34:22 +0200

    Create __init__.py
    file creation

---
+++ esr_package/__init__.py
@@ -0,0 +1,17 @@
+from .esr_zips import (
+    ESR,
+    ESRExtractor,
+    ESRZipProcessor,
+    ESRAnonymizer,
+    ESRExporter,
+    ESRPipeline,
+)
+
+__all__ = [
+    "ESR",
+    "ESRExtractor",
+    "ESRZipProcessor",
+    "ESRAnonymizer",
+    "ESRExporter",
+    "ESRPipeline",
+]
